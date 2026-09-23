c41927b1982959c0aef8cc72dbe00462038af6d4
RabeyaMuna <rabeyakhatun.muna@mail.concordia.ca>
2026-09-23 16:42:06 -0400

    xuyisen__diff__id_41

--- tests/test_config.py
+++ tests/test_config.py
@@ -74,6 +74,15 @@ def test_SSLContext_with_get_request(server, cert_pem_file):
     assert response.status_code == 200
 
 
+def test_ssl_context_repr():
+    context = httpx.SSLContext()
+    assert repr(context) == "<SSLContext [verify=True]>"
+
+    context = httpx.SSLContext(verify=False)
+    assert repr(context) == "<SSLContext [verify=False]>"
+
+
+
 def test_limits_repr():
     limits = httpx.Limits(max_connections=100)
     expected = (
